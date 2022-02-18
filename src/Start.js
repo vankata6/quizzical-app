eafcb9d15f9e1116fe25ab6ebaeb1127c9fdbfab
vankata6 <ikkolev95@gmail.com>
2022-02-18 18:09:34 +0200

    creating start page

---
+++ src/Start.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Start() {
+    return(
+        <div className="start-page">
+            <h1 className="header">Quizzical</h1>
+            <h3 className="question-text">App to check your common knowledge</h3>
+            <button className="check-btn">Start quiz</button>
+        </div>
+    )
+}
