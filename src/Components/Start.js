fc62a2f3c5b7ddb33bb7ce4b3eda92368ea187ba
vankata6 <ikkolev95@gmail.com>
2022-02-23 17:57:39 +0200

    navigate to quiz page from start

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom'
 
 export default function Start() {
+    const navigate = useNavigate()
+
     return(
         <div className="start-page">
             <h1 className="header">Quizzical</h1>
             <h3 className="start-header">App to check your common knowledge</h3>
-            <button className="check-btn">Start quiz</button>
+            <button 
+                className="check-btn"
+                onClick={() => navigate('/quiz')}    
+            >Start quiz</button>
         </div>
     )
 }
