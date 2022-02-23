a7f40426024085727ecd459e95f6c05a6fc354b9
vankata6 <ikkolev95@gmail.com>
2022-02-23 16:55:21 +0200

    create route to questions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
-// import data from "./data";
 import Start from "./Components/Start";
-// import Question from "./Components/Question";
+import Quiz from "./Components/Quiz";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
-//   const questionElements = data.map(quiz => {
-//     return <Question 
-//             question={quiz.question} 
-//             correct_answer={quiz.correct_answer} 
-//             incorrect_answers={quiz.incorrect_answers}
-//           />
-// })
 
   return (
     <Router>    
       <Routes>
         <Route exact path="/" element={<Start/>} />
+        <Route exact path="/quiz" element={<Quiz/>} />
       </Routes>
     </Router>
   )
---
+++ src/Components/Quiz.js
@@ -0,0 +1,20 @@
+import React from "react";
+import data from "../data";
+import Question from './Question';
+
+export default function Quiz() {
+      const questionElements = data.map(quiz => {
+    return <Question 
+            question={quiz.question} 
+            correct_answer={quiz.correct_answer} 
+            incorrect_answers={quiz.incorrect_answers}
+          />
+})
+
+return(
+    <div>
+        {questionElements}
+        <button className="check-btn">Check Answers</button>
+    </div>
+)
+}
