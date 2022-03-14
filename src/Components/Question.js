f806fbd0a671ad854eca23bcff5ff5c4b11c6fd1
vankata6 <ikkolev95@gmail.com>
2022-03-14 16:47:56 +0200

    highlight marked answer

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -5,8 +5,8 @@ export default function Question(props) {
     return (
         <div className="question">
             <h3 className="question-text">{props.question}</h3>
-            <button className="correct-answer-btn">{props.correct_answer}</button> 
-            <button className="incorrect-answer-btn">{props.incorrect_answers}</button>
+            <button className="answer-btn">{props.correct_answer}</button> 
+            <button className="answer-btn">{props.incorrect_answers}</button>
             <hr className="underline"/>
         </div>
     )
