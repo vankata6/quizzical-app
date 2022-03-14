f7f49c4aae024071ba34a54d3030787a4aa8b01c
vankata6 <ikkolev95@gmail.com>
2022-03-14 16:25:42 +0200

    fetching data from the API

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,15 +1,24 @@
-import React from "react";
-import data from "../data";
+import React, { useState, useEffect } from "react";
 import Question from './Question';
 
 export default function Quiz() {
-      const questionElements = data.map(quiz => {
+
+  const [object, setObject] = useState({})
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=boolean')
+    .then(res => res.json())
+    .then(data => setObject(data))
+  }, [])
+
+  const questionElements = object.results?.map(quiz => {
     return <Question 
             question={quiz.question} 
             correct_answer={quiz.correct_answer} 
             incorrect_answers={quiz.incorrect_answers}
+            key={quiz.question}
           />
-})
+  })
 
 return(
     <div>
