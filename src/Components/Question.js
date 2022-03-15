cce3623005186123483060bcf1c4c54e13cbe5e1
vankata6 <ikkolev95@gmail.com>
2022-03-15 17:39:21 +0200

    highlight marked buttons - repair

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 
-
 export default function Question(props) {
+    const [isLeftActive, setLeftActive] = useState("false");
+    const [isRightActive, setRightActive] = useState("false");
+
+    const handleLeftToggle = () => {
+      setLeftActive(!isLeftActive);
+    };
+
+    const handleRightToggle = () => {
+      setRightActive(!isRightActive);
+    };
 
     return (
         <div className="question">
             <h3 className="question-text">{props.question}</h3>
-            <button className='answer-btn'>{props.correct_answer}</button> 
-            <button className='answer-btn'>{props.incorrect_answers}</button>
+            <button 
+                className={isLeftActive ? 'answer-btn' : 'marked-btn'}
+                onClick={handleLeftToggle}
+            >{props.correct_answer}</button> 
+            <button 
+                className={isRightActive ? 'answer-btn' : 'marked-btn'}
+                onClick={handleRightToggle}
+            >{props.incorrect_answers}</button> 
             <hr className="underline"/>
         </div>
     )
