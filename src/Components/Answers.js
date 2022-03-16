935eca8b22a5e04c1732023d9c6d09698572503e
vankata6 <ikkolev95@gmail.com>
2022-03-16 16:19:43 +0200

    rerouting correction

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -1,35 +1,12 @@
-import React from "react";
 import Question from "./Question";
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Answers() {
-
-    const [object, setObject] = useState({})
     const navigate = useNavigate();
-  
-    useEffect(() => {
-      fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=boolean')
-      .then(res => res.json())
-      .then(data => setObject(data))
-    }, [])
-  
-    const questionElements = object.results?.map(quiz => {
-      return <Question 
-              question={quiz.question} 
-              correct_answer={quiz.correct_answer} 
-              incorrect_answers={quiz.incorrect_answers}
-              key={quiz.question}
-            />
-    })
-  
+
   return(
       <div>
-          {questionElements}
-          <button 
-            className="check-btn"
-            onClick={() => navigate('/')} 
-          >Play Again</button>
+          <Question/>
       </div>
   )
 }
--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -5,30 +5,45 @@ import { useNavigate } from "react-router-dom";
 export default function Quiz() {
 
   const [object, setObject] = useState({})
+  const [buttonText, setButtonText] = useState(false);
+  const [count, setCount] = useState(0);
   const navigate = useNavigate();
-
+  
+  const questionElements = object.results?.map(quiz => {
+    return <Question 
+    question={quiz.question} 
+    correct_answer={quiz.correct_answer} 
+    incorrect_answers={quiz.incorrect_answers}
+    key={quiz.question}
+    />
+  })
+  
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=boolean')
     .then(res => res.json())
     .then(data => setObject(data))
   }, [])
-
-  const questionElements = object.results?.map(quiz => {
-    return <Question 
-            question={quiz.question} 
-            correct_answer={quiz.correct_answer} 
-            incorrect_answers={quiz.incorrect_answers}
-            key={quiz.question}
-          />
+  
+  useEffect(() => {
+    const start = () => navigate('/'); 
+    if(count === 2) { start(); }
   })
 
-return(
+  function checkAnswers() {
+    setButtonText(!buttonText)
+  }
+
+  function playAgain() {
+  setCount(count + 1)
+  }
+  
+  return(
     <div>
         {questionElements}
         <button 
           className="check-btn"
-          onClick={() => navigate('/answers')} 
-        >Check Answers</button>
+          onClick={() => {checkAnswers(); playAgain()}}
+        >{buttonText ? "Play Again" : "Check Answers"}</button>
     </div>
 )
 }
